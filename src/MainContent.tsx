0dacbd52765e05eec7fca97292e3aedfa723c369
shoobah <shoobah@gmail.com>
2017-10-07 15:01:58 +0200

    Random chart

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -1,18 +1,23 @@
 import * as React from 'react';
 import Paper from 'material-ui/Paper';
 import MyChart from './MyChart';
+import RandomData from './RandomData';
 
 interface MainContentProps {}
+const rnd = new RandomData();
+const getData = () => {
+  let d = [];
+  for (let i = 0; i < 5; i++) {
+    d.push(rnd.getRandomData());
+  }
+  return { columns: d };
+};
 
 const MainContent: React.SFC<MainContentProps> = props => {
   return (
     <Paper zDepth={2} style={{ minHeight: '200px' }}>
       <div>
-        <MyChart
-          data={{
-            columns: [['data1', 30, 200, 10, 400, 150, 250], ['data2', 50, 20, 250, 40, 15, 25]]
-          }}
-        />
+        <MyChart data={getData()} />
       </div>
     </Paper>
   );
